0e6c617a189222daea7566b259cf7c7efb6ffb44
Vinier09 <engemarc187@gmail.com>
2026-04-10 01:57:45 +0200

    Fix Rotating
    Cover now stops/starts rotating if song is interrupted via another device

--- update.js
+++ update.js
@@ -1,4 +1,6 @@
 
+setInterval(checkisPlaying, 1000); // Aktualisiert den Wiedergabestatus jede Sekunde
+setInterval(isplaying, 1000);    // Überprüft den Wiedergabestatus jede Sekunde
 setInterval(updateCover, 1000);       // Aktualisiert das Cover jede Sekunde
 setInterval(switch_playPause, 1000);  // Aktualisiert den Play/Pause-Button jede Sekunde
 
